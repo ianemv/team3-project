f4c571b3153994169c66b34daa844703d6e13e00
Ian Villanueva <ian.villanueva@thesalesmachine.com>
2024-03-22 14:55:03 -0400

    added auth for authors

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -4,15 +4,15 @@ import errorHandler from './error.controller.js'
 const create = async (req, res) => { 
 const user = new User(req.body) 
 try {
-await user.save()
-return res.status(200).json({ 
-message: "Successfully signed up!"
-})
-} catch (err) {
-return res.status(400).json({
-error: errorHandler.getErrorMessage(err) 
-})
-} 
+	await user.save()
+	return res.status(200).json({ 
+		message: "Successfully signed up!"
+	})
+	} catch (err) {
+		return res.status(400).json({
+		error: errorHandler.getErrorMessage(err) 
+	})
+	}
 }
 const list = async (req, res) => { 
 try {
--- app/routes/author.router.js
+++ app/routes/author.router.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import authorController from '../controllers/author.controller.js';
+import authCtrl from '../controllers/auth.controller.js';
 
 const router = express.Router();
-
+console.log(authCtrl.requireSignin, 'authore')
 // Routes for authors
-router.post('/', authorController.createAuthor);
-router.get('/', authorController.getAllAuthors);
-router.get('/:id', authorController.getAuthorById);
-router.put('/:id', authorController.updateAuthor);
-router.delete('/:id', authorController.deleteAuthor);
-
+router.route('/').post(authCtrl.requireSignin, authorController.createAuthor);
+router.route('/').get(authCtrl.requireSignin, authorController.getAllAuthors);
+router.route("/:id")
+		.get(authCtrl.requireSignin, authorController.getAuthorById)
+		.put(authCtrl.requireSignin, authorController.updateAuthor)
+		.delete(authCtrl.requireSignin, authorController.deleteAuthor);
 
 export default router;
