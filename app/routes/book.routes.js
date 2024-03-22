99f94829ec007eb4e0ad3a4864061863c8df87f2
bytebard8 <peeksinthepast@gmail.com>
2024-03-22 16:15:01 -0400

    added authentication to book and user routes

--- app/routes/book.routes.js
+++ app/routes/book.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import bookController from "../controllers/book.controller.js";
+import authCtrl from '../controllers/auth.controller.js';
 const router = express.Router();
 
-router.post('/', bookController.createBook);
-router.get('/', bookController.getAllBooks);
-router.get('/:id', bookController.getBookById);
-router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.post('/', authCtrl.requireSignin, bookController.createBook);
+router.get('/', authCtrl.requireSignin, bookController.getAllBooks);
+router.get('/:id', authCtrl.requireSignin, bookController.getBookById);
+router.put('/:id', authCtrl.requireSignin, bookController.updateBook);
+router.delete('/:id', authCtrl.requireSignin, bookController.deleteBook);
 
 
 export default router;
