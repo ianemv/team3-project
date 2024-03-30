26fa00732338cbb5c8a6d7fffb0309bfe4f47f8a
Ian Villanueva <ian.villanueva@thesalesmachine.com>
2024-03-30 12:28:37 -0400

    update book saving

--- app/controllers/borrowings.controller.js
+++ app/controllers/borrowings.controller.js
@@ -1,5 +1,8 @@
 // borrowingController.js
+import mongoose from 'mongoose';
 import Borrowing from '../models/borrowings.model.js';
+import Book from '../models/book.model.js'; 
+import extend from 'lodash/extend.js'
 
 // Fetch all borrowings
 export const getAllBorrowings = async (req, res) => {
@@ -15,21 +18,20 @@ export const getAllBorrowings = async (req, res) => {
 export const borrowBook = async (req, res) => {
     try {
         const { borrowerId, bookId } = req.body;
-        console.log(req.body)
         // Create a new borrowing record
         const borrowing = await Borrowing.create({ borrowerId, bookId });
-        // Update book status to "borrowed"
-        const book = await Book.findByIdAndUpdate(
-            bookId,
-            { status: 'borrowed' }, 
-            { isAvailable: false }
-        );
 
-        res.status(201).json({ success: true, borrowing });
+        let book = await Book.findById(bookId);
+        // Update book status to "borrowed" and isAvailable to false
+        book = extend(book, { status: "borrowed", available: false })
+        await book.save();
+
+        res.status(201).json({ success: true, borrowing, book });
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
     }
 };
+
 export const returnBook = async (req, res) => {
     try {
         const { borrowingId } = req.params;
@@ -41,18 +43,15 @@ export const returnBook = async (req, res) => {
             { new: true }
         );
 
-        if (!borrowing) {
-            return res.status(404).json({ success: false, message: 'Borrowing record not found' });
+        if (!borrowing || borrowing.returned) {
+            return res.status(404).json({ success: false, message: 'Borrowing record not found or already returned' });
         }
-
         // Update book status to "available"
-        const book = await Book.findByIdAndUpdate(
-            borrowing.bookId,
-            { status: 'available' }, // Assuming there's a field named 'status' in your Book model
-            { isAvailable: true }
-        );
+        let book = await Book.findById(borrowing.bookId);
+        book = extend(book, { status: "available", available: true })
+        await book.save();
 
-        res.status(200).json({ success: true, borrowing });
+        res.status(200).json({ success: true, borrowing, book });
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
     }
