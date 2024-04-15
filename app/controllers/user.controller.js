9552dbb72f19945d3deba9578696990f1f9b8818
Ian Villanueva <ian.villanueva@thesalesmachine.com>
2024-04-14 22:01:04 -0400

    fix edit user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -45,19 +45,19 @@ req.profile.salt = undefined
 return res.json(req.profile) 
 }
 const update = async (req, res) => { 
-try {
-let user = req.profile
-user = extend(user, req.body) 
-user.updated = Date.now() 
-await user.save()
-user.hashed_password = undefined 
-user.salt = undefined
-res.json(user) 
-} catch (err) {
-return res.status(400).json({
-error: errorHandler.getErrorMessage(err) 
-})
-} 
+	try {
+		let user = req.profile
+      user = extend(user, req.body) 
+      user.updated = Date.now() 
+      await user.save()
+      user.hashed_password = undefined 
+      user.salt = undefined
+      res.json(user) 
+		} catch (err) {
+      return res.status(400).json({
+      error: errorHandler.getErrorMessage(err) 
+		})
+	} 
 }
 const remove = async (req, res) => { 
 	try {
