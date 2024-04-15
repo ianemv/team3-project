7077ef40d89bb724e2c7db8f18cc545884d01c88
Ian Villanueva <ian.villanueva@thesalesmachine.com>
2024-04-14 21:11:42 -0400

    include other fields in fetch users

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -15,14 +15,14 @@ try {
 	}
 }
 const list = async (req, res) => { 
-try {
-let users = await User.find().select('name email updated created') 
-res.json(users)
-} catch (err) {
-return res.status(400).json({
-error: errorHandler.getErrorMessage(err) 
-})
-} 
+	try {
+		let users = await User.find().select('name email updated created role first_name last_name');
+		res.json(users)
+	} catch (err) {
+			return res.status(400).json({
+			error: errorHandler.getErrorMessage(err) 
+		})
+	} 
 }
 const userByID = async (req, res, next, id) => { 
 try {
